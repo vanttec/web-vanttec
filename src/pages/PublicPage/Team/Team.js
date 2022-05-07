85eaa10de1dbed0245aad462bf32d72e394a7b8f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-06 19:24:38 -0500

    Agregado despliegue de miembros

--- src/pages/PublicPage/Team/Team.js
+++ src/pages/PublicPage/Team/Team.js
@@ -152,6 +152,40 @@ export default function Team() {
         facebook: "https://www.facebook.com/profile.php?id=100008118908014",
         twitter: "https://twitter.com/davidmtzhi",
       },
+    },
+    {
+      matricula: "A01235692",
+      nombre: "David Gerardo",
+      apellido: "Martínez Hidrogo",
+      datosPersonales: {
+        carrera: "ITC",
+        semestre: "4to",
+        birthplace: "Torreón, Coahuila",
+        birthday: "21 Enero 2000",
+        genero: "M",
+      },
+      infoGeneral: {
+        proyectos: {
+          roboboat: false,
+          robosub: false,
+          sdv: true,
+          drone: false,
+        },
+        puesto: "Web Developer",
+        entryYear: "2022",
+        area: "Media",
+      },
+      contacto: {
+        profilePicture: { David },
+        emailInstitucional: "a01235692@tec.mx",
+        emailPersonal: "gerardomartinez.hi@gmail.com",
+        whatsapp: "8712759212",
+        linkedin: "",
+        github: "https://github.com/davidmartinezhi",
+        instagram: "https://www.instagram.com/davidmartinez.hi/",
+        facebook: "https://www.facebook.com/profile.php?id=100008118908014",
+        twitter: "https://twitter.com/davidmtzhi",
+      },
     }
   ];
 
@@ -188,7 +222,7 @@ export default function Team() {
         </AreaBanner>
       </Row>
 
-      <Row className="members-list">
+      <Row className="members-list mt-5">
         <MembersList members={testMembers} area={"Media"} />
       </Row>
 
