8381ba69bea6b81f18e2b13f805fa2d42d6b9bab
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-12 14:42:15 -0500

    creada estructura basica

--- src/components/Public/Home/AboutUs/AboutUs.js
+++ src/components/Public/Home/AboutUs/AboutUs.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import {Container, Row} from 'react-bootstrap'
-import { Parallax } from "react-parallax";
+import {Container, Row, Col} from 'react-bootstrap'
 
-//background photos
-import pattern from "../../../../assets/img/jpg/andrew-kliatskyi-r4HT6aurLQw-unsplash.jpg";
+import './AboutUs.scss'
 
 export default function AboutUs() {
   return (
-    <Parallax strength={800} bgImage={pattern}>
       <Container fluid>
         <Row className='home-header'>
-          <h1>About Us</h1>
+          <h1>About</h1>
         </Row>
-        <Row></Row>
+        <Row className='about'>
+          <div className='about-description'>
+            <h4>Group of students focused on research and development of Autonomous Vehicles.</h4>
+          <p>Aiming to inspire young people to get involved in science and technology.
+            </p>
+          </div>
+        </Row>
+        <Row className='about-stats'>
+          <Col>
+            <span>2017</span>
+            <h4>Founded</h4>
+          </Col>
+          <Col>
+            <span>#</span>
+            <h4>Members</h4>
+          </Col>
+          <Col>
+            <span>#</span>
+            <h4>Published Papers</h4>
+          </Col>
+          <Col>
+            <span>#</span>
+            <h4>Fact/Stat</h4>
+          </Col>
+          
+        </Row>
       </Container>
-    </Parallax>
   );
 }
