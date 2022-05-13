b1dbfbad38c8bc20ad2d38b644af2bf49bc3105c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-12 21:21:23 -0500

    agregadas tarjetas a sponsors

--- src/components/Public/Home/Sponsors/Sponsors.js
+++ src/components/Public/Home/Sponsors/Sponsors.js
@@ -1,23 +1,90 @@
-import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import { Parallax } from "react-parallax";
 
 //Photos
-import demo from '../../../../assets/img/jpg/backgroundwhite.png'
+import demo from "../../../../assets/img/jpg/backgroundwhite.png";
 
-import './Sponsors.scss'
+//Logos
+import googleLogo from "../../../../assets/img/png/googleLogo.png";
+import tecLogo from "../../../../assets/img/png/tecLogo.png";
+
+import "./Sponsors.scss";
 
 export default function Sponsors() {
   return (
-    <Parallax strength={-100} bgImage={demo}>
-        <Container fluid>
-            <Row className='home-header'>
-                <h1>Supported By</h1>
-            </Row>
-            <Row>
-
-            </Row>
-        </Container>
+    <Parallax strength={-200} bgImage={demo}>
+      <Container fluid>
+        <Row className="home-header">
+          <h1>All Of This Is Possible Thanks To Our Sponsors</h1>
+        </Row>
+        <Row>
+          {/* Tec de monterrey */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={tecLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* Google */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* NVidia */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* Robonation */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* Akky */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* IFM */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* Akky */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* IFM */}
+          <Col xs={6} md={3}>
+            <Card className="sponsor-card">
+              <Card.Body className="sponsor-card-body">
+                <img className="sponsor-card-logo" src={googleLogo} alt="google logo" />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </Parallax>
-  )
+  );
 }
