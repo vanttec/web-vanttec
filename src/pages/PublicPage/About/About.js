be76c04bd5fb7825c3db5735f068f97b6ca746e0
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-18 17:16:19 -0500

    about us iniciado

--- src/pages/PublicPage/About/About.js
+++ src/pages/PublicPage/About/About.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Banner from "../../../components/Public/Banner/Banner";
+import Footer from "../../../components/Public/Footer/Footer";
+import MissionVision from "../../../components/Public/About/MissionVision/MissionVision";
+import Values from "../../../components/Public/About/Values/Values";
+
+//video
+import demo from "../../../assets/videos/mp4/VanttecDroneMural.mp4";
 
 export default function About() {
   return (
-    <div>About...</div>
-  )
+    <Container fluid>
+      {/* Main Banner */}
+      <Row className="main-banner">
+        <Banner video={demo}>
+          <h1>Welcome!</h1>
+        </Banner>
+      </Row>
+      {/* Content */}
+      <div className="content">
+        <div className="division" />
+        <MissionVision />
+        <div className="division" />
+        <Values />
+        <div className="division" />
+        <Footer />
+      </div>
+    </Container>
+  );
 }
