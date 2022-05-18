65b431c22383173463a3b111b515e92d4a5b1dd4
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-18 17:16:30 -0500

    initial setup values

---
+++ src/components/Public/About/Values/Values.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Parallax } from "react-parallax";
+
+import "./Values.scss";
+
+export default function Values() {
+  return (
+    <div>Values</div>
+  )
+}
