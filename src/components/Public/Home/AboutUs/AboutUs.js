dd840676f99059dd2868a0291881a7096b7ba6fe
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-18 23:01:49 -0500

    agregada nueva sección about

--- src/components/Public/Home/AboutUs/AboutUs.js
+++ src/components/Public/Home/AboutUs/AboutUs.js
@@ -1,79 +1,94 @@
-import React from 'react'
-import {Container, Row, Col, Card, Button} from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Parallax } from "react-parallax";
 import demo from "../../../../assets/img/jpg/whiteandgray.png";
-
-import './AboutUs.scss'
+import aboutPhoto from "../../../../assets/img/jpg/competition2.jpg";
+import "./AboutUs.scss";
 
 export default function AboutUs() {
   return (
     <Parallax strength={200} bgImage={demo}>
-      <Container fluid style={{display:"flex", flexDirection:"column", alignItems:"center"}} className="hover-effect">
-        <Row className="home-header">
-          <h1>About</h1>
-        </Row>
-        <Row className="about">
-          <div className="about-description">
-            <h4>
-              Student Group Focused on the Research and Development of Autonomous Vehicles for International Competitions and the Advancement of Technology
-            </h4>
-            <p>
-              Founded in 2017, VantTec aims to inspire young 
-              people to get involved in science and
-              technology, to build lasting relationships, and above all, to make dreams come true
-            </p>
-            <Button href='/about'>Learn More</Button>
-          </div>
-        </Row>
-        <Row className="about-stats">
-          <Col className="about-stats-col">
-            <Card className="about-stats-card">
-              <Card.Body>
-                <Card.Text>
-                  Founded
-                </Card.Text>
-                <Card.Title className="mb-2">
-                  <h2>2017</h2>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="about-stats-col">
-            <Card className="about-stats-card">
-              <Card.Body>
-                <Card.Text>
-                  Members
-                </Card.Text>
-                <Card.Title className="mb-2">
-                  <h2>76</h2>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="about-stats-col">
-            <Card className="about-stats-card">
-              <Card.Body>
-                <Card.Text>
-                  Papers Published
-                </Card.Text>
-                <Card.Title className="mb-2">
-                  <h2>12</h2>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="about-stats-col">
-            <Card className="about-stats-card">
-              <Card.Body>
-                <Card.Text>
-                  Awards
-                </Card.Text>
-                <Card.Title className="mb-2">
-                  <h2>18</h2>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
+      <Container
+        fluid
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        className="hover-effect"
+      >
+        <Row className="about-us-home">
+          <Card className="about-us-home-card">
+            <Card.Body>
+              <Row className="about-us-home-data">
+                <Col xs={12} md={6} className="mission-vision-data-col">
+                  <Card.Title>
+                    <h2 style={{ color: "black" }}>About</h2>
+                  </Card.Title>
+                  <Card.Text>
+                    <p>
+                      Our Vision is to Become one of the main exponents of autonomous vehicles in
+                      Mexico.
+                    </p>{" "}
+                    <p>
+                      Inspire young people to get involved in research in areas
+                      related to science and technology. In addition, help them
+                      learn about national and international opportunities in
+                      academic and professional fields.
+                    </p>
+                    <Button className="mb-3" href="/about">
+                      Learn More
+                    </Button>
+                  </Card.Text>
+                </Col>
+                <Col xs={12} md={6}>
+                  <Card.Img src={aboutPhoto} alt="vision" />
+                </Col>
+              </Row>
+              <Row className="about-stats">
+                <Col className="about-stats-col">
+                  <Card className="about-stats-card">
+                    <Card.Body>
+                      <Card.Text>Founded</Card.Text>
+                      <Card.Title className="mb-2">
+                        <h2>2017</h2>
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="about-stats-col">
+                  <Card className="about-stats-card">
+                    <Card.Body>
+                      <Card.Text>Members</Card.Text>
+                      <Card.Title className="mb-2">
+                        <h2>76</h2>
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="about-stats-col">
+                  <Card className="about-stats-card">
+                    <Card.Body>
+                      <Card.Text>Papers Published</Card.Text>
+                      <Card.Title className="mb-2">
+                        <h2>12</h2>
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="about-stats-col">
+                  <Card className="about-stats-card">
+                    <Card.Body>
+                      <Card.Text>Awards</Card.Text>
+                      <Card.Title className="mb-2">
+                        <h2>18</h2>
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
         </Row>
       </Container>
     </Parallax>
