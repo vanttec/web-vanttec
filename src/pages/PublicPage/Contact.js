fe30eaf2cdbd05278e4997424a33685db2e0abac
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-20 18:34:44 -0500

    contact initial setup

--- src/pages/PublicPage/Contact.js
+++ src/pages/PublicPage/Contact.js
@@ -1,9 +1,25 @@
 import React from "react";
+import {Container, Row} from "react-bootstrap";
+import Banner from "../../components/Public/Banner/Banner";
+import ContactInfo from "../../components/Public/Contact/ContactInfo";
+import Footer from "../../components/Public/Footer/Footer";
 
-export default function Contact(){
-    return(
-        <div>
-            <h1>Contact...</h1>
-        </div>
-    );
+//video
+import videoCampus from "../../assets/videos/mp4/VanttecCampusVideoo.mp4";
+
+export default function Contact() {
+  return (
+    <Container fluid>
+      <Row className="main-banner">
+        <Banner video={videoCampus}>
+          <h1>Contact</h1>
+        </Banner>
+      </Row>
+
+      <div className="content">
+        <ContactInfo />
+        <Footer />
+      </div>
+    </Container>
+  );
 }
