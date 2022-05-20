0390d2c5c5286fe2c6da2f220199c6e5db55daec
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-20 18:28:53 -0500

    initial setup Contact

---
+++ src/components/Public/Contact/ContactInfo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+export default function ContactInfo() {
+  return (
+    <Container fluid>
+        <Row>
+            {/* Contact Info */}
+            <Col xs={12} md={6}>
+                
+            </Col>
+
+            {/* Map */}
+            <Col xs={12} md={6}>
+                
+            </Col>
+
+        </Row>
+    </Container>
+  )
+}
