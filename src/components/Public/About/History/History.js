3bd365cdaa9e527b3d761ae97d51f5288c1478d3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-23 14:19:26 -0500

    cambio de estilo en las captions

--- src/components/Public/About/History/History.js
+++ src/components/Public/About/History/History.js
@@ -18,11 +18,11 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 The team emerged as a multidisciplinary project with
                 undergraduate and graduate students interested in researching
                 new technologies revolutionizing the field of STEM.
-              </h3>
+              </p>
               <h2>2017</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -33,11 +33,11 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 With graduate student Pedro Fonseca as the leader, together with
                 Juan Carlos Aguilera, the team was formed and the first project
                 was developed: an autonomous unmanned boat.
-              </h3>
+              </p>
               <h2>2017</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -47,10 +47,10 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 The team traveled to Florida to compete internationally, where
                 after this first competition, a legacy began.
-              </h3>
+              </p>
               <h2>2017</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -60,11 +60,11 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 In this way, the small experiment done by the team members
                 became a recognized group of autonomous vehicle research on
                 campus.
-              </h3>
+              </p>
               <h2>2017</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -74,12 +74,12 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 With young leaders, Alex González
                 and Pedro Sánchez, first time in charge. This was a major
                 challenge for the team. As all of the older members graduating,
                 the team required to set the ground for further generations.
-              </h3>
+              </p>
               <h2>2018</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -89,11 +89,11 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 What was the result? A second time competing internationally
                 against top universities, going to Florida with all of our flaws
                 to prove that we could keep on... And won an award.{" "}
-              </h3>
+              </p>
               <h2>2018</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -103,10 +103,10 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 The first time we reached the finals. The team finished in 5th
                 place after a great performance in the qualifiers.
-              </h3>
+              </p>
               <h2>2019</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -116,12 +116,12 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 Proud of ourselves. We advanced miles in comparison to
                 the previous years. Our technology and our team had evolved.
                 Under the leadership of Alex, we became a solid group and our
                 research started to bloom. Setting us up for 2020.
-              </h3>
+              </p>
               <h2>2019</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -131,9 +131,9 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
               Due to the COVID-19 pandemic, RoboBoat and RoboSub 2020 resorted to being celebrated online without an on-place competition and social-distancing measures encouraged.
-              </h3>
+              </p>
               <h2>2020</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -143,10 +143,10 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
                 The Team Achieved 1st place Overall Standing in the Roboboat competition and 3th place in Robosub.
                 Including Other 6 Awards.
-              </h3>
+              </p>
               <h2>2020</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -156,9 +156,9 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
               We were awarded the Rómulo Garza award for our focus on researching issues related to autonomous vehicles.
-              </h3>
+              </p>
               <h2>2020</h2>
             </Carousel.Caption>
           </Carousel.Item>
@@ -168,9 +168,9 @@ export default function History() {
             </div>
 
             <Carousel.Caption>
-              <h3>
+              <p>
               Due to the COVID-19 pandemic, As we continue to be on lockdown, VantTec continued operations throughout online meetings and deliveries through virtual testing.             
-              </h3>
+              </p>
               <h2>2021</h2>
             </Carousel.Caption>
           </Carousel.Item>
