a9f8070dc41828164a631059b5d4aa4d6850729f
nadia.co <ngarciaorozco@outlook.com>
2022-05-24 17:53:50 -0500

    members styling + standardization

--- src/components/Public/Team/MembersInfoModal/MembersInfoModal.js
+++ src/components/Public/Team/MembersInfoModal/MembersInfoModal.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Offcanvas,
-  Carousel,
-  Card,
-} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import ModalBanner from "../ModalBanner/ModalBanner";
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, Offcanvas, Carousel, Card } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import ModalBanner from '../ModalBanner/ModalBanner'
 import {
   faWhatsapp,
   faLinkedin,
@@ -17,34 +10,32 @@ import {
   faInstagram,
   faFacebook,
   faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons'
 
-import "./MembersInfoModal.scss";
-
+import './MembersInfoModal.scss'
 
 export default function MembersInfoModal(props) {
-  const { show, setShow, membersData, index, handleSelect, video } = props;
-  const [activeIndex, setActiveIndex] = useState(index);
+  const { show, setShow, membersData, index, handleSelect, video } = props
+  const [activeIndex, setActiveIndex] = useState(index)
 
   useEffect(() => {
-    setActiveIndex(index);
-  }, [index]);
+    setActiveIndex(index)
+  }, [index])
 
   return (
     <>
       <Offcanvas
-        style={{ height: "100%" }}
+        style={{ height: '100%' }}
         show={show}
         onHide={() => setShow(false)}
         placement="bottom"
       >
         <Offcanvas.Header
-          style={{ display: "flex", justifyContent: "flex-end" }}
+          style={{ display: 'flex', justifyContent: 'flex-end' }}
           closeButton
         ></Offcanvas.Header>
         <Container>
           <ModalBanner video={video}>
-          
             <Carousel
               fade
               indicators={false}
@@ -52,96 +43,91 @@ export default function MembersInfoModal(props) {
               onSelect={handleSelect}
             >
               {membersData.map((member) => (
-                
                 <Carousel.Item className="carousel-item-member">
                   <Row className="carousel-item-member-row">
-                  <Col xs={12} md={6} className="member-img">
-                    <img
-                      src={member.profilePicture}
-                      alt={`${member.nombre} ${member.apellido}`}
-                    />
-                  </Col>
-                  <Col xs={12} md={6} className="info-modal">
-                    <Card className="member-info-card">
-                      <Card.Header as="h1">{`${member.nombre} ${member.apellido}`}</Card.Header>
-                      <Card.Body className="member-card-body">
-                      <div className="member-bio">
-                        <Card.Title as="h4">Bio</Card.Title>
-                        <Card.Text>
-                          {member.bio}
-                          </Card.Text></div>
+                    <Col xs={12} md={6} className="member-img">
+                      <img
+                        src={member.profilePicture}
+                        alt={`${member.nombre} ${member.apellido}`}
+                      />
+                    </Col>
+                    <Col xs={12} md={6} className="info-modal">
+                      <Card className="member-info-card">
+                        <Card.Header as="h1">{`${member.nombre} ${member.apellido}`}</Card.Header>
+                        <Card.Body className="member-card-body">
+                          <div className="member-bio">
+                            {/* <Card.Title as="h4">Bio</Card.Title> */}
+                            <Card.Text>{member.bio}</Card.Text>
+                          </div>
                           <div className="member-personal-data">
-                          <Card.Title as="h4">About</Card.Title>
-                          <Card.Text>
-                          
-                            Area: {member.infoGeneral.area}
-                            <br />
-                            Position: {member.infoGeneral.puesto}
-                            <br />
-                            Projects: <br />
-                            Career: {member.datosPersonales.carrera} <br />
-                            Semester: {member.datosPersonales.semestre} <br />
-                            Birthplace: {member.datosPersonales.birthplace}{" "}
-                            <br />
-                          
-                          </Card.Text>
+                            <Card.Title as="h4">About</Card.Title>
+                            <Card.Text>
+                              Position: {member.infoGeneral.puesto}
+                              <br />
+                              Area: {member.infoGeneral.area}
+                              <br />
+                              {/* Projects: <br /> */}
+                              {member.datosPersonales.carrera} |{' '}
+                              {member.datosPersonales.semestre}th Semester{' '}
+                              <br />
+                              Birthplace: {
+                                member.datosPersonales.birthplace
+                              }{' '}
+                              <br />
+                            </Card.Text>
                           </div>
                           <div className="member-contact-card">
-                          <Card.Text>
-                          
-                          <Card.Title as="h4">Contact</Card.Title>
-                          <ContactInfo member={member} />
-                          
-                          </Card.Text>
+                            <Card.Text>
+                              <Card.Title as="h4">Contact</Card.Title>
+                              <ContactInfo member={member} />
+                            </Card.Text>
                           </div>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-
+                        </Card.Body>
+                      </Card>
+                    </Col>
                   </Row>
                 </Carousel.Item>
               ))}
             </Carousel>
-            
           </ModalBanner>
-          </Container>
+        </Container>
       </Offcanvas>
     </>
-  );
+  )
 }
 
 function ContactInfo(props) {
-  const { member } = props;
+  const { member } = props
 
   return (
     <Row>
       <Col xs={12} md={5} className="member-social-media">
         <div>
-        <a href={member.contacto.linkedin} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
+          <a href={member.contacto.linkedin} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
 
-        <a href={member.contacto.github} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
+          <a href={member.contacto.github} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
 
-        <a href={member.contacto.instagram} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a href={member.contacto.facebook} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-        <a href={member.contacto.twitter} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
+          <a href={member.contacto.instagram} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href={member.contacto.facebook} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href={member.contacto.twitter} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
         </div>
       </Col>
       <Col xs={12} md={7} className="member-contact-information">
         <a href="">
-      <span>
+          <span>
             {/* <FontAwesomeIcon icon={faWhatsapp} />
             <p>{member.contacto.whatsapp}</p> */}
-        </span>
+          </span>
         </a>
         <a href={`mailto:${member.contacto.emailInstitucional}`}>
           <span>
@@ -157,5 +143,5 @@ function ContactInfo(props) {
         </a>
       </Col>
     </Row>
-  );
+  )
 }
